423f2e64b18d6782a57c8a9c169d8b6acc11cca7
Greg Marvin <gregmarv@gmail.com>
2018-02-20 12:56:33 -0700

    holyjesus

--- server/index.js
+++ server/index.js
@@ -1,16 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session=require('express-session');
+const createInitialSession=require('./middlewares/session');
+let filter=require('./middlewares/filter');
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        secure:false,
+        maxAge:10000
+    }
+}))
+
+app.use(createInitialSession);
+// app.use((req,res,next)=>{
+//     const {method}=req;
+//     method==="POST"||method==="PUT"?filter(req,res,next):next();
+// })
+
+
 
 const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
+app.post( messagesBaseUrl, filter, mc.create );
 app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}`, mc.update );
+app.put( `${messagesBaseUrl}`, filter, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
 
 const port = process.env.PORT || 3000
---
+++ server/middlewares/filter.js
@@ -0,0 +1,8 @@
+let notAllowed=['moist','mustard','half-life-3']
+module.exports=function(req,res,next){
+    while ( notAllowed.find( word => req.body.text.includes(word) ) ) {
+        const badWord = notAllowed.find( word => req.body.text.includes(word) );
+        req.body.text = req.body.text.replace( badWord, '*'.repeat( badWord.length ) );
+      }
+      next();
+}
